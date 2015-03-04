cd66eb92d70488f407e911f029f3dc5c7f99a7ff
Igor Ramos <igor.f.ramos@gmail.com>
2015-03-03 21:18:25 -0300

    Do the final step of the Codelab. It does not look beautiful, but our prototype works and we know how to write a simple Android app now! =)

--- TempoAgora/app/src/main/java/com/codelab/android/tempoagora/CityWeatherActivity.java
+++ TempoAgora/app/src/main/java/com/codelab/android/tempoagora/CityWeatherActivity.java
@@ -1,10 +1,13 @@
 package com.codelab.android.tempoagora;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +15,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 
 public class CityWeatherActivity extends ActionBarActivity {
@@ -55,22 +71,80 @@ public class CityWeatherActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private View rootView;
+        private String cityName;
+        private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_city_weather, container, false);
-
-            setCityName(rootView);
+            rootView = inflater.inflate(R.layout.fragment_city_weather, container, false);
+            cityName = getCityName();
+            setCityName();
             return rootView;
         }
 
-        private void setCityName(View view) {
-            TextView textView = (TextView) view.findViewById(R.id.cityName);
-            Intent intent = getActivity().getIntent();
-            textView.setText(intent.getStringExtra(TempoAgoraMainActivity.CITY_NAME));
+        @Override
+        public void onResume() {
+            super.onResume();
+            AsyncTask<Void, Void, String> getWeatherTask = new AsyncTask<Void, Void, String>() {
+                @Override
+                protected String doInBackground(Void... params) {
+                    try {
+                        return getWeatherForCity();
+                    } catch (IOException e) {
+                        Log.e(getClass().getName(), e.getMessage(), e);
+                    }
+                    Toast.makeText(getActivity(),
+                            R.string.connection_failed_error_message,
+                            Toast.LENGTH_LONG).show();
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    TextView resultTextView = (TextView) rootView.findViewById(R.id.resultTextView);
+                    resultTextView.setText(s);
+                }
+            };
+            getWeatherTask.execute();
+        }
+
+        private String getCityName() {
+            return getActivity().getIntent().getStringExtra(TempoAgoraMainActivity.CITY_NAME);
+
+        }
+
+        private void setCityName() {
+            TextView textView = (TextView) rootView.findViewById(R.id.cityName);
+            textView.setText(cityName);
+        }
+
+        private String getWeatherForCity() throws IOException {
+            URL url = new URL(BASE_URL + URLEncoder.encode(cityName, "UTF-8"));
+            Log.d("URL to connect", url.toString());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream stream = connection.getInputStream();
+                return getStringFromInputStream(stream);
+            } else {
+                throw new IllegalStateException("Error getting data from OpenWeatherMap.");
+            }
+        }
+
+        private static String getStringFromInputStream(InputStream inputStream) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String response = "";
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response += line;
+            }
+            return response;
         }
     }
 }
